e7831ed72dc044fedf4ae15815ec65f1e41c37d5
Abishen <100848571+AbishenAG@users.noreply.github.com>
2025-07-14 19:07:54 +0100

    Delete version.tf

--- version.tf
+++
@@ -1,4 +0,0 @@
-# Tells Terraform which versions of the Terraform CLI are allowed to run this code. required_version = ensures compatible Terraform CLI version. 
-terraform {
-  required_version = ">= 1.0.0"
-}
